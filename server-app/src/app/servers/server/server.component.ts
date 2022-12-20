d7166ff2d6ecb11faae75d97eaa2fe33b9b210ef
Tatiana Mraz <tatiana.mrazova@icloud.com>
2022-12-20 09:45:38 +0100

    using query parameters to allow app to edit

--- server-app/src/app/servers/server/server.component.ts
+++ server-app/src/app/servers/server/server.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'})
+  }
 }
