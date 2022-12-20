4fa61e3e48ae09a21b8b5b99ca36db493c204b24
Tatiana Mraz <tatiana.mrazova@icloud.com>
2022-12-20 08:06:30 +0100

    navigating with router links, setting navigation paths, styling active router links

--- server-app/src/app/servers/servers.component.ts
+++ server-app/src/app/servers/servers.component.ts
@@ -4,7 +4,7 @@ import { ServersService } from './servers.service';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
